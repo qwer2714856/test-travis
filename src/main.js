903eccdd4295214e1e4fbdb64562dad74c75b01a
luchao.ding <116710782@qq.com>
2019-07-24 14:53:03 +0800

    fix: 1.0.0-development

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import App from './App'
-     //eee tttt   bbbbb aaa aa aaa    aaa              
+     //eee tttt kkkkkkkkkk llllllllll   bbbbb aaa aa aaa    aaa              
 Vue.config.productionTip = false
 //       
 /* eslint-disable no-new */
